418f83cb166ec35dfc95bd3c5e522c271dac50bf
ikercs <ikercsv@gmail.com>
2026-02-23 16:43:07 -0600

    Fixed copilot syntax

--- simulation/strategy.js
+++ simulation/strategy.js
@@ -32,43 +32,32 @@ export function strategy(robots, ball, canvas, side) {
         const canvas_midpoint = (canvas.height / constants.SCALE) / 2;
         const field_width = canvas.width / constants.SCALE;
 
-        if (side === 'left') {
-            if (x > constants.GOAL.RIGHT_PADDING) {
-        // Determine desired movement direction components for this frame
         let dx = 0;
         let dy = 0;
 
         if (side === 'left') {
             if (x > constants.GOAL.RIGHT_PADDING) {
-                // Move left to stay within right padding
                 dx = -1;
             } else if (x < constants.GOAL.LEFT_PADDING) {
-                // Move right to stay within left padding
                 dx = 1;
             }
         } else {
             if (x < (field_width - constants.GOAL.RIGHT_PADDING)) {
-                // Move right to stay within right padding (from the right side)
                 dx = 1;
             } else if (x > (field_width - constants.GOAL.LEFT_PADDING)) {
-                // Move left to stay within left padding (from the right side)
                 dx = -1;
             }
         }
 
         if (y < (canvas_midpoint - constants.GOAL.MIDPOINT_OFFSET)) {
-            // Too far up; move down
             dy = 1;
         } else if (y > (canvas_midpoint + constants.GOAL.MIDPOINT_OFFSET)) {
-            // Too far down; move up
             dy = -1;
         }
 
-        // Apply combined movement (possibly diagonal) once per frame
         if (dx !== 0 || dy !== 0) {
             const move_angle = Math.atan2(dy, dx) * 180 / Math.PI;
             defender.move(constants.BASE_SPEED, move_angle);
         }
     }
 }
-
